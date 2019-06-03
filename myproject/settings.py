79d154a28ef67685a3a9a1d478634b4561064fd9
Guido Franco <guidofranco-@hotmail.com>
2019-06-03 13:03:57 -0300

    deploy

--- myproject/settings.py
+++ myproject/settings.py
@@ -124,6 +124,6 @@ STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
